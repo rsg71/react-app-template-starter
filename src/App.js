d47650f902e26d14eb1c687dba038a5a97fbba02
Robert Greenawalt <robert.greenawalt29@gmail.com>
2023-07-23 19:50:44 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className="App-welcome-text">
+          Edit <code>src/App.js</code> and save to reload. ABC here's the app.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
